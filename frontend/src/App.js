0e0b389bb2eb71d4c64187878680779cd343971d
Ari Moshe <arimoshe@gmail.com>
2022-09-18 13:45:26 -0700

    add navbar Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,25 @@
+import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+import { Switch } from 'react-router-dom';
+
+import MainPage from './components/MainPage/MainPage';
+import LoginForm from './components/SessionForms/LoginForm';
+import SignupForm from './components/SessionForms/SignupForm';
+import NavBar from './components/NavBar/NavBar';
+
+
+
+
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <NavBar />
+      <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/login" component={LoginForm} />
+        <AuthRoute exact path="/signup" component={SignupForm} />
+      </Switch>
+    </>
   );
 }
 
